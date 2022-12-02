46634fd79f24d84842defdd7cf59af8c265c025a
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-12-02 20:22:37 +0100

    Limpio un poco el codigo del frontend y añado comentarios detallados.

--- public/firebase.js
+++ public/firebase.js
@@ -1,124 +1,122 @@
-// Necesito importar la libreria
-    console.log('firebase js')
-    import { initializeApp,  } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js';
-    import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword   } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
-    //import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore-lite.js';
-    const firebaseConfig = {
-        apiKey: "AIzaSyDZHR2iFUpIUF_Ojn73RAj6xBTGBs_jytg",
-        authDomain: "eliminarlo2.firebaseapp.com",
-        projectId: "eliminarlo2",
-        storageBucket: "eliminarlo2.appspot.com",
-        messagingSenderId: "347973977710",
-        appId: "1:347973977710:web:f55cf297019b3b4cba487f",
-        measurementId: "G-9CBE4J8EBV"
-    };
+// Librerias de firebase que se encargan de conectarme con la API de Autenticacion de Firebase
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
 
-      
-    
-    var token; 
+// Archivo de configuración de firebase con mis credenciales
+const firebaseConfig = {
+  apiKey: "AIzaSyDZHR2iFUpIUF_Ojn73RAj6xBTGBs_jytg",
+  authDomain: "eliminarlo2.firebaseapp.com",
+  projectId: "eliminarlo2",
+  storageBucket: "eliminarlo2.appspot.com",
+  messagingSenderId: "347973977710",
+  appId: "1:347973977710:web:f55cf297019b3b4cba487f",
+  measurementId: "G-9CBE4J8EBV",
+};
 
-    const app = initializeApp(firebaseConfig);
-    const auth = getAuth(app);
+// Inicio los módulos de Firebase passandole como parámetro las credenciales definidas en firebaseConfig.
+const app = initializeApp(firebaseConfig);
 
+// Inicialitzo el módul de auth
+const auth = getAuth(app);
 
+// Método para crear un usuario nuevo
 
+const crearUsuarioNuevo = async (email, password) => {
+  const prueba = await createUserWithEmailAndPassword(auth, email, password);
+};
 
+// Método para loguear a un usuario
+const loguearUsuario = async (email, password) => {
+  // REcibo mail y password como parámetros y se los paso al método signIn de Firebase auth
+  console.log("loguear usuario", email, password);
+  const usuario = await signInWithEmailAndPassword(auth, email, password);
+  console.log("usuario", usuario);
 
+  // Accedo al token que esta en la propiedad token de la propiedad user
+  token = usuario.user.accessToken;
 
-    // Get a list of cities from your database
+  // Si hay token es que me he podido loguear correctamente
+  console.log("token USUARIO EXITOSAMENTE LOGUEADO", token);
+  cargarTodo(token);
+};
 
-    const crearUsuarioNuevo = async (email, password) =>{
-        const prueba = await createUserWithEmailAndPassword(auth, email, password)
-        console.log('prueba', prueba)
-    }
-
-  const loguearUsuario = async (email, password) =>{
-        console.log('loguear usuario', email, password)
-        const usuario = await signInWithEmailAndPassword(auth, email, password)
-        console.log('us', usuario)
-        token = usuario.user.accessToken
-        console.log('token USUARIO EXITOSAMENTE LOGUEADO', token)
-        cargarTodo()
-    }
-
-    
-
-    const cargarTodo = ( )=>{
-        console.log('token desde index', token)
-        fetch(`http://localhost:3002/users`,
-        { 
-          method: 'get', 
-          headers: new Headers({
-              'Authorization': token
-          }), 
+const cargarTodo = (token) => {
+  fetch(`http://localhost:3002/users`, {
+    method: "get",
+    headers: new Headers({
+      Authorization: token,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Recibo la data del backend con el array de usuarios o un error en caso de que no este autorizado a entrar
+      console.log("data", data);
+      if (data.message) {
+        // SI la data porta una propietat message afegida vol dir que no estaba autoritzat i mostro un alert amb el missatge
+        alert(data.message);
+        // A mes, retorno false perque no vull que pugui continuar
+        return false;
+      } else {
+        // Altrament retorno la data
+        return data;
       }
+    })
+    .then((users) => {
+      // Monto el HTML, añado el titulo y los usuarios a la lista
+      addTitle();
+      addList(users);
+    });
 
-        )
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("data", data);
-          if(data.message){
-            alert(data.message)
-            return false
-          }else{
-            return data;
-          }
-          
-          
-        })
-        .then((users) => {
-          console.log("users", users);
-          
-          addTitle();
-          addList(users);
-        });
-      
-      function addTitle() {
-        // Creo un DIV element
-        const newDiv = document.createElement("div");
-      
-        // Le pongo un título
-        const newContent = document.createTextNode("Listado de Usuarios");
-        newDiv.appendChild(newContent);
-      
-        // Lo inserto al DOM
-        const currentDiv = document.getElementById("div1");
-        document.body.insertBefore(newDiv, currentDiv);
-      }
-      
-      function addList(users) {
-        // Creo un elemento UL
-        const ul = document.createElement("ul");
-      
-        // Itero sobre cada usuario
-        users.forEach((user) => {
-          // En cada usuario creo un elemento LI y le añado el nombre
-          const li = document.createElement("li");
-          li.appendChild(document.createTextNode(user.nombre));
-      
-          // Añado el elemento LI al elemento UL
-          ul.appendChild(li);
-        });
-      
-        // Lo inserto al DOM
-        const currentDiv = document.getElementById("div1");
-        document.body.insertBefore(ul, currentDiv);
-      }
-      }
-      
+  // La función addTitle añade el titulo
+  function addTitle() {
+    // Creo un DIV element
+    const newDiv = document.createElement("div");
 
+    // Le pongo un título
+    const newContent = document.createTextNode("Listado de Usuarios");
+    newDiv.appendChild(newContent);
 
+    // Lo inserto al DOM
+    const currentDiv = document.getElementById("div1");
+    document.body.insertBefore(newDiv, currentDiv);
+  }
 
-//window.loguearUsuario = loguearUsuario
+  // Esta función añade el listado de usuarios
+  function addList(users) {
+    // Creo un elemento UL
+    const ul = document.createElement("ul");
 
-document.querySelector('button').addEventListener('click', ()=>{
-  
+    // Itero sobre cada usuario
+    users.forEach((user) => {
+      // En cada usuario creo un elemento LI y le añado el nombre
+      const li = document.createElement("li");
+      li.appendChild(document.createTextNode(user.nombre));
 
-  const email = document.getElementById("email").value
-  const password = document.getElementById("password").value
-  console.log('email y password', email, password)
-  loguearUsuario(email, password)
+      // Añado el elemento LI al elemento UL
+      ul.appendChild(li);
+    });
+
+    // Lo inserto al DOM
+    const currentDiv = document.getElementById("div1");
+    document.body.insertBefore(ul, currentDiv);
+  }
+};
+
+// Añado un event listener ( escuchador de eventos ) click.
+
+document.querySelector("button").addEventListener("click", () => {
+  // Cojo el email y el password del html
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  console.log("email y password", email, password);
+
+  // Cuando tengo el email y el password llamo a la función loguearUsuario
+  loguearUsuario(email, password);
 });
-   
- 
