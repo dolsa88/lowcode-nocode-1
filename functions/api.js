3108feaf010206306242f3fddadf902b9316fef8
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-12-02 20:38:59 +0100

    Preparo los condicionales para que se ejecute una parte del codigo u otra en función de si la versión es de producción o es la versión local.

--- functions/api.js
+++ functions/api.js
@@ -1,26 +1,45 @@
+// Inicializo express ( la libreri que nos permite montar la API)
 const express = require("express");
+
+// Cors nos permite evitar el error de CORS
 const cors = require("cors");
-const axios = require("axios")
+
+// Cargo libreria de firebase
+const admin = require("firebase-admin");
+
+// Cargo el archivo json que me informa de si la versión es la de produccion (fireebase functions) o es en local (localhost)
+const local = require("./version.json").local;
 
 // Inicializacion Firestore Database
 //var passwords = require("./cert.json");
 // const admin = require("firebase-admin");
+if (local) {
+  // Carrego les credencials
+  var passwords = require("./cert.json");
+  admin.initializeApp({
+    credential: admin.credential.cert(passwords),
+  });
+} else {
+  admin.initializeApp();
+}
 
-var passwords = require("./cert.json");
-const admin = require("firebase-admin");
-admin.initializeApp({
-  credential: admin.credential.cert(passwords)
-});
-
-//admin.initializeApp();
-
+// Inicialitzo el modul de Base de Dades
 const db = admin.firestore();
-const auth = admin.auth();
-const app = express();
-app.use(cors());
-app.use(express.json());
-const port = 3002;
 
+// Inicializo el modul de Authorization de Firebase
+const auth = admin.auth();
+
+// Inicializo la app de Express
+const app = express();
+
+// Le paso el modulo cors para evitar errores de cors
+app.use(cors());
+
+// Le informo a express que vamos a trabajar con json
+app.use(express.json());
+
+// Defino el puerto local
+const port = 3002;
 
 // Este metodo se encarga de decidir si un usuario puede acceder o no
 const authentication = async (token) => {
@@ -29,16 +48,15 @@ const authentication = async (token) => {
   try {
     const respuesta = await auth.verifyIdToken(token);
     // Dins de la resposta hi ha una propietat email
-    const email = respuesta.email
-    console.log('email', email)
+    const email = respuesta.email;
+    console.log("email", email);
 
     // SI es l'usuari juanito@gmail.com no pot accedir per aixo retorno false altramanera poden accedir
-    if(email === "juanito@gmail.com"){
-      return false
-    }else{
-      return true
+    if (email === "juanito@gmail.com") {
+      return false;
+    } else {
+      return true;
     }
-   
   } catch (e) {
     // Si hemos llegado a este bloque es porque el metodo verifyIdToken dio error
     // Es decir el usuario o el token o lo que sea no era correcto
@@ -48,15 +66,14 @@ const authentication = async (token) => {
   // SI es correcto devuelvo un true y si no es correcto devuelvo un false
 };
 
-
 // Middleware que s'encarrega de comprobar avans que entrin en joc els endpoints si l'usuari pot accedir o no
-app.use( async (req, res, next) => {
-  if (! req.headers.authorization) {
-    return res.status(403).json({ error: 'No has añadido token!' });
+app.use(async (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(403).json({ error: "No has añadido token!" });
   }
 
   // SI llego aqui es porque si me han añadido un token.
-  const token = req.headers.authorization
+  const token = req.headers.authorization;
 
   // Le paso el token al metodo authentication que me dira si puede pasar o no
   const puedePasar = await authentication(token);
@@ -64,26 +81,19 @@ app.use( async (req, res, next) => {
   // Si no puede pasar, le diremos que "No estas autorizadoo a entrar"
   if (!puedePasar) {
     return res.status(401).send({ message: "No estás autorizado a entrar" });
-  }else{
+  } else {
     // Aixó es un metode nadiu d'express que diu que puedes continuar
-  next()
+    next();
   }
-    
 });
-
-
-
 
 // Endpoint del HOME
 app.get("/", (req, res) => res.send("Hola mundo"));
 
-
 // OPERACIONS Create Read Update Delete
-
 
 // OPERACIO READ ALL USERS
 app.get("/users", async (req, res) => {
-
   // COnexión con la base de datos para leer todos los usuarios
   const users = await db.collection("usuarios").get();
 
@@ -103,7 +113,6 @@ app.get("/user/:id/", async (req, res) => {
   res.send(user.data());
 });
 
-
 // OPERACIO CREATE ONE USER
 app.post("/user", async (req, res) => {
   const user = req.body;
@@ -112,7 +121,6 @@ app.post("/user", async (req, res) => {
   await db.collection("usuarios").add(user);
   res.send({ result: "success" });
 });
-
 
 // OPERACIO UPDATE ONE USER AMB METODE PUT
 app.put("/user/:id", async (req, res) => {
@@ -127,7 +135,6 @@ app.put("/user/:id", async (req, res) => {
 
   res.send({ result: "success" });
 });
-
 
 // OPERACIO UPDATE ONE USER AMB METODE PATCH
 app.patch("/user/:id", async (req, res) => {
@@ -153,8 +160,12 @@ app.delete("/user/:id", async (req, res) => {
   res.send({ result: "success" });
 });
 
-app.listen(port, () => {
-  console.log(`Desde api.js ${port}`);
-});
-
-//module.exports = app;
+if (local) {
+  // Si trabajamos en local necesito inicializar el servidor
+  app.listen(port, () => {
+    console.log(`Desde api.js ${port}`);
+  });
+} else {
+  // SI no trabajamos en local, necesito exportar el módulo
+  module.exports = app;
+}
